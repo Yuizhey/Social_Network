de5c217d3dd4123f89c33a5fc77d2edf34ffdb7d
Yuizhey <Gre4ka28@yandex.ru>
2025-04-17 16:53:47 +0300

    доделал все основные вьюшки на sidebar

---
+++ SocialNetwork/SocialNetwork/Controllers/EventsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Controllers;
+
+public class EventsController : Controller
+{
+    public EventsController()
+    {
+        
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
